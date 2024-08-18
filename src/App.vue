80a73b1735ca8ddc6359496a571846ba21ae9f32
Orlov Viacheslav <viacheslavorlov@gmail.com>
2024-08-18 23:10:10 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div>Работает</div>
+  <button>click me</button>
+  <input type="text" class="border w-full" />
+  <textarea class="border" name="" id="" cols="30" rows="10"></textarea>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script></script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+<style scoped>
+.border {
+  border: 1px black solid;
+  background: #eee;
 }
 </style>
